375fe3f5d6df3f8d197d0d596ba96036d91fbcad
leviklomp <85936160+leviklomp@users.noreply.github.com>
2021-06-22 00:38:12 +0200

    Update MetropolisHastings.jl

--- MetropolisHastings.jl
+++ MetropolisHastings.jl
@@ -24,19 +24,16 @@ function MetropolisHastingsAlgorithm(posterior_density,initial_θ,transition_den
         v_z = posterior_density(θ_new)
 
         if logvariant
-            #log version
-            #this is done because this can cause errors (not clear why)
-
-
+            #log version (to prevent numerical underflow)
+            
             q_ratio_log = q_z_to_y - q_y_to_z
             v_ratio_log = v_z - v_y
-            #println([q_y_to_z,q_z_to_y,q_ratio_log])
-            #println([v_y,v_z,v_ratio_log])
+           
             log_α = v_ratio_log+q_ratio_log
             #println(log_α)
             u = rand(Uniform(0,1))
 
-            if log(u) < log_α
+            if log(u) < log_α #acceptance
                 println("ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT")
                 θ_i = θ_new
                 acceptance += 1
@@ -49,12 +46,11 @@ function MetropolisHastingsAlgorithm(posterior_density,initial_θ,transition_den
             end
             q_ratio = q_z_to_y/q_y_to_z
             v_ratio = v_z/v_y
-            println([q_y_to_z,q_z_to_y,q_ratio])
-            println([v_y,v_z,v_ratio])
+           
             α = v_ratio*q_ratio
             u = rand(Uniform(0,1))
-
-            if u < α
+          
+            if u < α #acceptance
                 println("ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT ACCEPT")
                 θ_i = θ_new
                 acceptance += 1
@@ -66,36 +62,3 @@ function MetropolisHastingsAlgorithm(posterior_density,initial_θ,transition_den
     return θ_matrix[:,burnin:end],acceptance/n
 end
 
-## Needed for the transition density
-function MvNormalpdf(μ,Σ,y)
-    """Also works for non positive definite Σ (MvNormal does not!). Returns the logarithm"""
-    N = length(μ)
-    Det = det(Σ)
-
-    #((2*pi)^(N/2)*
-    #(1/((2*pi)^(N/2)*sqrt(abs(det(Σ)))))*exp(-0.5*(transpose(y-μ))*inv(Σ)*(y-μ)))
-    if Det == 0.0
-        factor = 1
-        while Det == 0.0
-            factor += 1
-            Det = det(Σ*factor)
-        end
-        global value = N*log(factor).-(N/2)*log(2*pi).-log(sqrt(abs(det(Σ*factor)))).-0.5*(transpose(y-μ))*inv(Σ)*(y-μ)
-    elseif Det == Inf
-        factor = 1
-        while Det == Inf
-            factor += 1
-            Det = det(Σ/factor)
-        end
-        global value = -N*log(factor)-(N/2)*log((2*pi)).-log(sqrt(abs(det(Σ*factor)))).-0.5*(transpose(y-μ))*inv(Σ)*(y-μ)
-    else
-        global value = -(N/2)*log(2*pi).-log(sqrt(abs(det(Σ)))).-0.5*(transpose(y-μ))*inv(Σ)*(y-μ)
-    end
-    return value[1]
-end
-
-function MvTDistpdf(d,μ,Σ,X) #dealing with underflow using eigenvalues
-    p = length(X)
-    val = loggamma((d+p)/2).-loggamma(d/2).-(p/2)*log(d*pi).-(1/2)*logdet(Complex.(Σ)).-0.5*(d+p)*log(Complex.(1 .+(1/d)*transpose(X-μ)*inv(Σ)*(X-μ)))
-    return real(val[1])
-end
